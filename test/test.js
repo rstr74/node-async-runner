2b622adec64865fe2a16942e5548da389c6e5e61
rstr74 <robbertstreng2012@gmail.com>
2014-09-05 21:21:21 +0200

    init

---
+++ test/test.js
@@ -0,0 +1,39 @@
+var AsyncRunner = require("../lib/async-runner").AsyncRunner;
+
+var myAsyncRunner = new AsyncRunner();
+
+
+myAsyncRunner.addStatusChangeListener(
+	function(event) {
+		switch (event.type) {
+			case AsyncRunner.FINISHED:
+				console.log("Event " + AsyncRunner.FINISHED);
+				break;
+		}
+	}
+);
+
+myAsyncRunner.addTask(function(callback, index, data) {
+	ready = function() {
+		console.log("Task " +index+" "+  data.text + " >FINISHED IN " + callback.getExecutionTime());
+		callback.next();
+	}
+	//simulate a time consuming task
+	setTimeout(ready, 500);
+}, {
+	text: "hello world !"
+});
+
+
+myAsyncRunner.addTask(function(callback, index, data) {
+	ready = function() {
+		console.log("Task " +index+" "+  data.text + " >FINISHED IN " + callback.getExecutionTime());
+		callback.next()
+	}
+	// simulate a time consuming task
+	setTimeout(ready, 1000);
+}, {
+	text: "goodbye world !"
+});
+
+myAsyncRunner.executeAll();
